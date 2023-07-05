99194dc34f3a48f626faee79e822b98b89e7c7b0
SidG2003 <sid.gupta0303@gmail.com>
2023-07-05 19:55:01 +0530

    created model

---
+++ model.py
@@ -0,0 +1,54 @@
+import os
+import torch
+import torchvision
+import tarfile
+from torchvision.datasets.utils import download_url
+from torch.utils.data import random_split
+
+from torchvision.datasets import ImageFolder
+from torchvision.transforms import ToTensor
+
+import matplotlib
+import matplotlib.pyplot as plt
+# %matplotlib inline
+
+from torch.utils.data.dataloader import DataLoader
+
+from torchvision.utils import make_grid
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+from ImageClassificationBase import ImageClassificationBase
+
+class Cifar10CnnModel(ImageClassificationBase):
+    def __init__(self):
+        super().__init__()
+        self.network = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2), # output: 64 x 16 x 16
+
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2), # output: 128 x 8 x 8
+
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2), # output: 256 x 4 x 4
+
+            nn.Flatten(), 
+            nn.Linear(256*4*4, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10))
+        
+    def forward(self, xb):
+        return self.network(xb)
--- neural.py
+++ neural.py
@@ -8,6 +8,20 @@ from torch.utils.data import random_split
 from torchvision.datasets import ImageFolder
 from torchvision.transforms import ToTensor
 
+import matplotlib
+import matplotlib.pyplot as plt
+# %matplotlib inline
+
+from torch.utils.data.dataloader import DataLoader
+
+from torchvision.utils import make_grid
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+from ImageClassificationBase import ImageClassificationBase
+from model import Cifar10CnnModel
+
 project_name='05-cifar10-cnn'
 
 # Dowload the dataset
@@ -36,4 +50,81 @@ dataset = ImageFolder(data_dir+'/train', transform=ToTensor())
 
 img, label = dataset[0]
 print(img.shape, label)
-print(img)
+# print(img)
+
+print(dataset.classes)
+
+matplotlib.rcParams['figure.facecolor'] = '#ffffff'
+
+def show_example(img, label):
+    print('Label: ', dataset.classes[label], "("+str(label)+")")
+    plt.imshow(img.permute(1, 2, 0))
+    plt.show()
+
+# show_example(*dataset[0])
+
+random_seed = 42
+torch.manual_seed(random_seed)
+
+val_size = 5000
+train_size = len(dataset) - val_size
+
+train_ds, val_ds = random_split(dataset, [train_size, val_size])
+print(len(train_ds), len(val_ds))
+
+batch_size=128
+
+train_dl = DataLoader(train_ds, 
+                      batch_size, 
+                      shuffle=True, 
+                    #   num_workers=4, 
+                      pin_memory=True)
+
+val_dl = DataLoader(val_ds, 
+                    batch_size*2, 
+                    # num_workers=4, 
+                    pin_memory=True)
+
+def show_batch(dl):
+    for images, labels in dl:
+        fig, ax = plt.subplots(figsize=(12, 6))
+        ax.set_xticks([]); ax.set_yticks([])
+        ax.imshow(make_grid(images, nrow=16).permute(1, 2, 0))
+        plt.show()
+        break
+
+# show_batch(train_dl)
+    
+model = Cifar10CnnModel()
+print(model)
+
+def get_default_device():
+    """Pick GPU if available, else CPU"""
+    if torch.cuda.is_available():
+        return torch.device('cuda')
+    else:
+        return torch.device('cpu')
+    
+def to_device(data, device):
+    """Move tensor(s) to chosen device"""
+    if isinstance(data, (list,tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
+class DeviceDataLoader():
+    """Wrap a dataloader to move data to a device"""
+    def __init__(self, dl, device):
+        self.dl = dl
+        self.device = device
+        
+    def __iter__(self):
+        """Yield a batch of data after moving it to device"""
+        for b in self.dl: 
+            yield to_device(b, self.device)
+
+    def __len__(self):
+        """Number of batches"""
+        return len(self.dl)
+    
+device = get_default_device()
+print(device)
